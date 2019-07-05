4318a60a2c0547a8d728a75c32b198af7dc2dc47
colorful9767 <1605333199@qq.com>
2019-07-05 13:45:16 +0800

    Override ListNode toString Method

--- src/Node/ListNode.java
+++ src/Node/ListNode.java
@@ -5,10 +5,22 @@ package Node;
  * @Desc Aefinition for singly-linked list
  */
 public class ListNode {
-    int val;
+    Integer val;
     ListNode next;
 
     public ListNode(int val) {
         this.val = val;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder().append(val);
+        ListNode nextNode = next;
+        while (nextNode != null) {
+            sb.append(" -> ").append(nextNode.val);
+            nextNode = nextNode.next;
+        }
+
+        return sb.toString();
     }
 }
