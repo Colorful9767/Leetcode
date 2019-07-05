575f20d68fe32e891e5f05c74560e3e4f63fceec
colorful9767 <1605333199@qq.com>
2019-07-05 13:26:47 +0800

    Add ListNode Class

---
+++ src/Node/ListNode.java
@@ -0,0 +1,14 @@
+package Node;
+
+/**
+ * Created by perl on 2019/7/5.
+ * @Desc Aefinition for singly-linked list
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
