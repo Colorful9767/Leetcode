dd31d2be9da7f1fb8d9b6d8bfdb5e50cb2137c63
colorful9767 <1605333199@qq.com>
2019-07-11 09:50:57 +0800

    Add TreeNode

---
+++ src/Node/TreeNode.java
@@ -0,0 +1,15 @@
+package Node;
+
+/**
+ * Created by perl on 2019/7/10.
+ */
+public class TreeNode<T> {
+      T val;
+      TreeNode left;
+      TreeNode right;
+
+    public TreeNode(T val) {
+        this.val = val;
+    }
+
+}
